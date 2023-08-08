c354ee956123b52d02ab3258c9aa1c5c90748c78
DE101 <dinganaearnestfohtung@gmail.com>
2023-08-08 07:11:37 +0100

    update signup

--- src/signup.js
+++ src/signup.js
@@ -1,7 +1,7 @@
 import React from "react";
 export default function Signup () {
     const [name, setName] = React.useState(0);
-    const [Email, setEmail] = React.useState(0);
+    const [email, setEmail] = React.useState(0);
     
 
     function updateName(elem) {
@@ -11,16 +11,22 @@ export default function Signup () {
         setEmail(elem.target.value);
     }
 
+    function submitForm(event) {
+        event.preventDefault();
+        const payLoad = {name, email};
+        console.log(payLoad);
+    }
+
     return (
      <form>
-           <div className="control">
-              <label> Name: </label> <input type="text" onChange={updateName} />
+           <div className="control" onSubmit={submitForm} >
+              <label> Name: </label> <input require type="text" onChange={updateName} />
            </div>           
            <div className="control">
-              <label> Email: </label> <input type="email" onChange={updateEmail} />
+              <label> Email: </label> <input require type="email" onChange={updateEmail} />
            </div>
            <div className="control">
-              <input type="submit"  />
+              <input type="submit" />
            </div>
 
      </form>
