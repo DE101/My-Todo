a6e36f44071c29b86e0fc5eb1532b4051c228c03
DE101 <dinganaearnestfohtung@gmail.com>
2023-08-08 06:52:02 +0100

    update email

--- src/signup.js
+++ src/signup.js
@@ -1,10 +1,28 @@
+import React from "react";
 export default function Signup () {
+    const [name, setName] = React.useState(0);
+    const [Email, setEmail] = React.useState(0);
+    
 
-    return {
+    function updateName(elem) {
+        setName(elem.target.value);
+    }
+    function updateEmail(elem) {
+        setEmail(elem.target.value);
+    }
+
+    return (
      <form>
            <div className="control">
-              <label> Name: </label> <input type="text" />
+              <label> Name: </label> <input type="text" onChange={updateName} />
+           </div>           
+           <div className="control">
+              <label> Email: </label> <input type="email" onChange={updateEmail} />
            </div>
+           <div className="control">
+              <input type="submit"  />
+           </div>
+
      </form>
-    };
+    );
 }
