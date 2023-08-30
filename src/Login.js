ffa5f635de7e9eff6aa599342d43d9ea06b76f72
DE101 <dinganaearnestfohtung@gmail.com>
2023-08-30 20:23:52 +0100

    added content

--- src/Login.js
+++ src/Login.js
@@ -1,3 +1,18 @@
 export default function Login() {
+    return (
+        <div>
+            <form>
+                <div>
+                    <label>username:</label> <input type="text" required placeholder="username"/>
+                </div>
+                <div>
+                    <label>username:</label> <input type="password" required placeholder="password"/>
+                </div>
+                <div>
+                    <button>Log In</button>
+                </div>
+            </form>
+        </div>
+    )
 
 };
--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,3 +1,20 @@
 export default function UserProfile() {
-
+    return (
+        <div>
+            <div>
+                <img src="./logo.svg" alt="profile-pic"/>
+            </div>
+            <div>
+                <label>User Name:</label>
+                <label>User Email:</label>
+                <label>User Number:</label>
+            </div>
+            <div>
+                <span></span>
+            </div>
+            <div>
+                <button>Switch Account</button>
+            </div>
+        </div>
+    )
 };
