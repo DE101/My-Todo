104983ef9499dbb4ac8472afa3e1aa2b78ff9512
DE101 <dinganaearnestfohtung@gmail.com>
2023-10-01 22:50:15 +0100

    added content

--- src/welcomePage.js
+++ src/welcomePage.js
@@ -4,7 +4,7 @@ export default function welcomePage() {
             <h1>Welcome</h1>
             <p>Welcome to the myToDo community</p>
             <p>Welcome to our app, please login to continue tracking your daily activities</p>
-            
+            <p>if you don't already have an account you can sign uo here.</p>
         </div>
     )
 };
