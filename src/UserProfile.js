632d9f5d8a5e82591ba161e1c6c4effd5eca8de6
DE101 <dinganaearnestfohtung@gmail.com>
2023-10-07 12:28:46 +0100

    added content

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,7 +1,7 @@
 export default function UserProfile() {
     return (
         <div>
-            <div>
+            <div className="profile-pic">
                 <img src="./logo.svg" alt="profile-pic"/>
             </div>
             <div>
