76557d1fd0a426003803e424a9d389e9c0f1a3ed
DE101 <dinganaearnestfohtung@gmail.com>
2024-01-23 21:36:49 +0100

    added content

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./App.css";
 import Todo from "./Todo";
 
-function TodoList ({ItemList, toggleTodo}) {
-    return (
-        <ul>
-            {ItemList.map((todo,index) => (
-            <Todo item={todo} iterationKey={index} toggle={toggleTodo} />
-            ))}
-        </ul>
-    );
+function TodoList({ ItemList, toggleTodo }) {
+  return (
+    <div className="saveContainer">
+      <ul>
+        {ItemList.map((todo, index) => (
+          <Todo item={todo} iterationKey={index} toggle={toggleTodo} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default TodoList;
