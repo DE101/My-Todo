3cb96e7e39867fb2e7561ff3e83723479b96c48d
DE101 <dinganaearnestfohtung@gmail.com>
2024-01-25 22:04:12 +0100

    modified content

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,8 @@
 export default function Details (props) {
     return (
         <div className="details">
-            <span className="description-text">{props.text}</span>
             <span className="material-symbols-outlined icon">{props.icon}</span>
+            <span className="description-text">{props.text}</span>
         </div>
     )
 }
