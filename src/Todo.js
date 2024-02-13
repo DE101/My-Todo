2f9cb5bad6fdd25864392f8395620159ab7fa884
DE101 <dinganaearnestfohtung@gmail.com>
2024-02-13 12:25:13 +0100

    added content

--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, useState from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"
 import "./App.css";
+import { useState } from "react";
 
 
 function Todo ({ item, iterationKey, toggle, remove}) {
+    const [selectedDate, setselectedDate] = useState(null);
+
+    const handleDateChange = (date) => {
+        setselectedDate(date);
+    }
     return (
                 <li className={item.done ? "done" : ""} key={iterationKey}> 
                 <input
@@ -16,6 +24,15 @@ function Todo ({ item, iterationKey, toggle, remove}) {
                 <br></br>
                 <button onClick={() => {
                     remove(iterationKey)}}><span className="material-symbols-outlined icon">delete</span></button>
+                    <DatePicker
+        className="datentime"
+        selected={selectedDate}
+        onChange={handleDateChange}
+        dateFormat="D/MM/YYYY - hh:mm"
+        showTimeSelect
+        timeIntervals={5}
+        timeFormat="hh:mm"
+        />
                 </li>
             );
 }
