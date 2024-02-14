2ebd1a82adb29ac20cac208e560a6ba6daf42d8b
DE101 <dinganaearnestfohtung@gmail.com>
2024-02-14 22:44:31 +0100

    arrage code structure

--- src/Todo.js
+++ src/Todo.js
@@ -1,30 +1,35 @@
 import React from "react";
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css"
+import "react-datepicker/dist/react-datepicker.css";
 import "./App.css";
 import { useState } from "react";
 
+function Todo({ item, iterationKey, toggle, remove }) {
+  const [selectedDate, setselectedDate] = useState(null);
 
-function Todo ({ item, iterationKey, toggle, remove}) {
-    const [selectedDate, setselectedDate] = useState(null);
-
-    const handleDateChange = (date) => {
-        setselectedDate(date);
-    }
-    return (
-                <li className={item.done ? "done" : ""} key={iterationKey}> 
-                <input
-                key={iterationKey}
-                 type="checkbox" 
-                 className="checkBox"
-                 onChange={() => {
-                    toggle(iterationKey);
-                }} /> 
-                {item.todoText}
-                <br></br>
-                <button onClick={() => {
-                    remove(iterationKey)}}><span className="material-symbols-outlined icon">delete</span></button>
-                    <DatePicker
+  const handleDateChange = (date) => {
+    setselectedDate(date);
+  };
+  return (
+    <li className={item.done ? "done" : ""} key={iterationKey}>
+      <input
+        key={iterationKey}
+        type="checkbox"
+        className="checkBox"
+        onChange={() => {
+          toggle(iterationKey);
+        }}
+      />
+      {item.todoText}
+      <br></br>
+      <button
+        onClick={() => {
+          remove(iterationKey);
+        }}
+      >
+        <span className="material-symbols-outlined icon">delete</span>
+      </button>
+      <DatePicker
         className="datentime"
         selected={selectedDate}
         onChange={handleDateChange}
@@ -32,9 +37,9 @@ function Todo ({ item, iterationKey, toggle, remove}) {
         showTimeSelect
         timeIntervals={5}
         timeFormat="hh:mm"
-        />
-                </li>
-            );
+      />
+    </li>
+  );
 }
 
 export default Todo;
