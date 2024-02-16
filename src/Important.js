5856e0e06cc53f24bcbaa837778281db187f695e
DE101 <dinganaearnestfohtung@gmail.com>
2024-02-16 11:43:45 +0100

    edited important component

--- src/Important.js
+++ src/Important.js
@@ -1,9 +1,18 @@
-export default function Important () {
+import TodoList from "./TodoList"
+
+export default function Important ({todos}) {
+
+    const importantList = todos.filter(todos => todos.important === true)
+    
     return (
         <div>
             <h1>
                 Important Tasks
             </h1>
+                <TodoList 
+                // {...importantList}
+                />
+            
         </div>
     )
 }
