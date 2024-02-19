4cb9f8f487383ea1daccfce68c31f7887832621d
DE101 <dinganaearnestfohtung@gmail.com>
2024-02-19 07:46:41 +0100

    new detail div

--- src/Details.js
+++
@@ -1,8 +0,0 @@
-export default function Details (props) {
-    return (
-        <div className="details">
-            <span className="material-symbols-outlined icon">{props.icon}</span>
-            <span className="description-text">{props.text}</span>
-        </div>
-    )
-}
--- src/Myday.js
+++ src/Myday.js
@@ -4,6 +4,10 @@ export default function Myday() {
     return (
         <div>
             <p>Tasks of the day</p>
+            <div className="details">
+            <span className="material-symbols-outlined icon">{props.icon}</span>
+            <span className="description-text">{props.text}</span>
+            </div>
         </div>
     );
 };
