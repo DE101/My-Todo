be880cbbdfac28f0b956b20b4ab6850bc57ccffa
DE101 <dinganaearnestfohtung@gmail.com>
2024-02-22 05:10:26 -0600

    edited Settings

--- src/Settings.js
+++ src/Settings.js
@@ -1,10 +1,16 @@
 export default function Settings() {
   return (
-    <div>
+    <div className="settingsContainer">
+      <h1>Settings</h1>
       <div className="displaySettings">
         <h3>Display</h3>
         <p>Wallpaper</p>
         <p>Font size</p>
+      </div>
+      <div className="ThemeSettings">
+        <h3>Theme</h3>
+        <p>Dark</p>
+        <p>Light</p>
       </div>
       <div className="notificationSettings">
         <h3>Notifications</h3>
@@ -13,7 +19,16 @@ export default function Settings() {
         <p>Volume</p>
         <p>Ring time duration</p>
       </div>
-      <div></div>
+      <div className="about">
+        <h3>About</h3>
+        <p>Wallpaper</p>
+        <p>Font size</p>
+      </div>
+      <div className="help">
+        <h3>Help Us</h3>
+        <p>Rate Us</p>
+        <p>Send us Feedback</p>
+      </div>
     </div>
-  ); 
+  );
 }
